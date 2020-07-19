4cbfbe98a386140d4d14df75a6e81ee970d6e98a
JamesF <jamesf@intuitivesystems.co.uk>
2020-07-19 09:52:38 +0100

    Updates

---
+++ precache-manifest.d8b13be62da8ae2f4ee4a837a2942fbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4607a540986acd4f793fcfe68a976947",
+    "url": "/react-bbcode-demo/index.html"
+  },
+  {
+    "revision": "08b4a99c7c52ea34ebcb",
+    "url": "/react-bbcode-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bed22a77787ba434124d",
+    "url": "/react-bbcode-demo/static/js/2.9e8baf99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bbcode-demo/static/js/2.9e8baf99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b4a99c7c52ea34ebcb",
+    "url": "/react-bbcode-demo/static/js/main.8dd942d7.chunk.js"
+  },
+  {
+    "revision": "05361fc10ddfc657b49d",
+    "url": "/react-bbcode-demo/static/js/runtime-main.afe5cd92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-bbcode-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
